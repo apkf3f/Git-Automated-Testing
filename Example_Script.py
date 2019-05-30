5f14e89fc2a236d3946261627a986fd55b1dd2ef
Adam Kraus <apkf3f@gmail.com>
2019-05-30 14:15:22 -0500

    Adding example py file

---
+++ Example_Script.py
@@ -0,0 +1,7 @@
+#####################################################################
+# Purpose: This is a test file for creating a useful script.		#
+# Programmer(s): Adam Kraus											#
+# Starting Date: 5.30.2019											#
+#																	#
+#####################################################################
+print("yaskawa")

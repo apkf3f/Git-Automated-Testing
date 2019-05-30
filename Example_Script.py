d043c621846a10b72f64b609a9e625152e352fb3
Adam Kraus <apkf3f@gmail.com>
2019-05-30 15:41:20 -0500

    Sparse Files succcesss. Rm & add updated .py

--- Example_Script.py
+++ Example_Script.py
@@ -5,13 +5,18 @@
 # Starting Date: 5.30.2019											#
 #																	#
 #####################################################################
-print("Adam Kraus: VCS Project")
 
+################################ IMPORTANT NOTE: Github recommended maximum file size = 50MB ############################################################
 #VARIABLES
-files = ["one","two","three"]
+
+
+#MAIN
+print("Adam Kraus: VCS Project\n")
+num_files = input("number of files needed: ")	#Get number of files for repo 
+file_size = input("Size of file needed(KB): ")	#Get file size in kilobytes
 
 #FUNCTION 1: MAKE X .txt FILES
-for file in files;
-	with open("%s_ex_file.txt","w+") as f: #var new file opens <new_file>, w+ says to make the file if it doesn't exist
-	f.write("Line 1")
-	f.write("Line 2")
+for index in range(int(num_files)):
+	with open("%u_ex_file.txt" %index,"w+") as file: #var new file opens <new_file>, w+ says to make the file if it doesn't exist
+		file.truncate(1024*int(file_size))	#Creates a sparse file of <file_size> KB's
+

153b31fbbad64c0f4b5a329e03dc2e1c800c9bc7
Adam Kraus <apkf3f@gmail.com>
2019-05-31 11:30:59 -0500

    Testing new commit style

--- Example_Script.py
+++ Example_Script.py
@@ -13,15 +13,16 @@
 
 #VARIABLES
 commit_msg = "automated commit message"
-
+commit_msg2 = "Automated remove of scripted files"
 #SUBPROCESS -> INIT
 import subprocess
 import time
+start_time = time.time()	#initial EPOCH
 PIPE = subprocess.PIPE
 branch = 'my_branch'
 process = subprocess.Popen(['git', 'pull', branch], stdout=PIPE, stderr=PIPE)
 stdoutput, stderroutput = process.communicate()
-subprocess.call(["git","status"])
+#subprocess.call(["git","status"])
 
 #USER DISPLAY & VARIABLE INPUT
 print("VCS Project\n")
@@ -30,19 +31,31 @@ num_files = int(num_files)
 file_size = input("Size of file needed(KB): ")	#Get file size in kilobytes - IMPORTANT NOTE: Github recommended maximum file size = 50MB
 file_size = int(file_size)
 
-
 # MAKE <num_files> .txt FILES of <file_size> size in KB
 for index in range(num_files):
 	with open("%u_ex_file.txt" %index,"w+") as file: #var new file opens <new_file>, w+ says to make the file if it doesn't exist
 		file.truncate(1024*file_size)	#Creates a sparse file of <file_size> KB's
 		#subprocess.call(["git","add","%u_ex_file.txt" %index]) #Add the <num_files>[index] for commit stage
-time.sleep(2)	#Gives the new files enough time to grow to size. Change to variable?
 
 #SUBPROCESS -> GIT COMMANDS
+start_add_cmd_time = time.time()
 for add_index in range(num_files):
 	subprocess.call(["git","add","%u_ex_file.txt" %add_index]) #Add the <num_files>[index] for commit stage
+end_add_cmd_time = time.time()
 
-subprocess.call(["git","status"])	#Check difference in what's added
+#subprocess.call(["git","status"])	#Check difference in what's added
 subprocess.call(["git","commit","-m","commit_msg"]) #Commit the new files to repo
 subprocess.call(["git","push","origin","master"]) #Push new files to repo
 subprocess.call(["git","status"])	#Check difference in what's pushed
+
+rm_files = input("Delete Scripted files (Y/N)? ")	#Get number of files for repo
+if rm_files == 'Y':
+	start_rm_cmd_time = time.time()
+	for rm_index in range(num_files):
+		subprocess.call(["git","rm","%u_ex_file.txt" %rm_index]) #remove the <num_files>[index] for commit stage
+	subprocess.call(["git","commit","-m",commit_msg2]) #Commit the new files to repo
+	subprocess.call(["git","push","origin","master"]) #Push new files to repo
+	end_rm_cmd_time = time.time()
+	print("Remove time: ", end_rm_cmd_time - start_rm_cmd_time)
+#DATA (Will move to text file creation)	
+print("Add time: ", end_add_cmd_time - start_add_cmd_time)
